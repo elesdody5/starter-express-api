62976c0c22f6b0c6d8ba15dd512740d57ef1b946
Ahmed Reda <manoa15@yahoo.com>
2022-11-12 18:34:07 +0200

    test2

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -308,17 +308,17 @@ exports.notifyAllUsers = catchAsync(async (req, res, next) => {
     // topic: "users",
   };
 
-  // if (userRegistrationTokens.length > 0) {
-  //   // sendMultipleNotification(userRegistrationTokens, message, "users", res);
-  //   for (let i = 0; i < userRegistrationTokens.length; i++) {
-  //     await sendNotification(userRegistrationTokens[i], message);
-  //   }
-  //   await Notification.create(req.body);
-  // }
-  console.log(userRegistrationTokens);
   if (userRegistrationTokens.length > 0) {
-    await sendMultipleNotification(userRegistrationTokens, message, "sss", res);
+    // sendMultipleNotification(userRegistrationTokens, message, "users", res);
+    for (let i = 0; i < userRegistrationTokens.length; i++) {
+      await sendNotification(userRegistrationTokens[i], message);
+    }
+    await Notification.create(req.body);
   }
+  console.log(userRegistrationTokens);
+  // if (userRegistrationTokens.length > 0) {
+  //   await sendMultipleNotification(userRegistrationTokens, message, "sss", res);
+  // }
   await Notification.create(req.body);
   res.status(200).json({
     status: "success",
