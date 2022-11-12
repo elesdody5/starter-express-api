18fd9fa94b709743ff7d98d12cab0e26b371115f
Ahmed Reda <manoa15@yahoo.com>
2022-11-12 21:46:46 +0200

    logging error for fcm

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -363,7 +363,7 @@ exports.notifySingleUser = catchAsync(async (req, res, next) => {
   };
   fcm.send(message, function (err, response) {
     if (err) {
-      console.log("Something has gone wrong!");
+      console.log("Something has gone wrong!", err);
     } else {
       console.log("Successfully sent with response: ", response);
     }
