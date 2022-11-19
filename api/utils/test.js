aeadc618a941458b90232ef49dfd41d47cd10edd
Ahmed Reda <manoa15@yahoo.com>
2022-11-19 11:25:35 +0200

    change file path in test

--- api/utils/test.js
+++ api/utils/test.js
@@ -2,7 +2,7 @@ const { Storage } = require("@google-cloud/storage");
 
 const storage = new Storage({
   projectId: "delivery-app-5e621",
-  keyFilename: "delivery-app-5e621-firebase-adminsdk-kjin7-465d741a9b.json",
+  keyFilename: "delivery-app-5e621-firebase-adminsdk-kjin7-392a4a1fae.json",
 });
 let bucket = storage.bucket("gs://delivery-app-5e621.appspot.com");
 
