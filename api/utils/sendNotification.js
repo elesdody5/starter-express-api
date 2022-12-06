d36b4ad7773a70b7b590d3c8bb0fde3d1874ab72
Ahmed Reda <manoa15@yahoo.com>
2022-12-06 22:55:13 +0200

    Require fcm

--- api/utils/sendNotification.js
+++ api/utils/sendNotification.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
 const axios = require("axios");
+var FCM = require("fcm-node");
 
 let serviceAcc = require("../../delivery-app-5e621-firebase-adminsdk-kjin7-392a4a1fae.json");
 let fcm = new FCM(serviceAcc);
